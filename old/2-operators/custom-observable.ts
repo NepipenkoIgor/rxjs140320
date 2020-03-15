cd6bbe129d7c1af9172fbad63fac88376bd81c12
Ihor <igornepipenko@gmail.com>
2020-03-15 13:00:49 +0200

    Feature: observable general creation  [ref:no-ref]

---
+++ old/2-operators/custom-observable.ts
@@ -0,0 +1,59 @@
+import { Observable, Subscriber } from 'rxjs'
+import { pluck } from "rxjs/operators";
+
+// const sequence = new Observable((subscriber: Subscriber<number>) => {
+//     let count = 1;
+//     const intervalId = setInterval(() => {
+//         subscriber.next(count++);
+//         if (count === 10) {
+//             subscriber.complete();
+//             clearInterval(intervalId);
+//         }
+//     }, 1000)
+// });
+//
+// setTimeout(() => {
+//     sequence.subscribe((value) => {
+//         console.log('Subscribe 2', value)
+//     })
+// }, 5000)
+//
+// sequence.subscribe((value) => {
+//     console.log('Subscribe 1', value)
+// })
+
+//конечный или нет
+// горячий или нет
+
+
+const socket: WebSocket = new WebSocket('wss://echo.websocket.org');
+const source$ = new Observable((subscriber) => {
+    socket.addEventListener('message', (e) => subscriber.next(e))
+    return () => {
+        console.log('unsubscribed')
+        // socket.close();
+    }
+})
+
+socket.addEventListener('open', () => {
+    let count = 0;
+    setInterval(() => {
+        socket.send((count++).toString())
+    }, 1000)
+})
+
+const subscription = source$
+    .pipe(pluck('data'))
+    .subscribe((message) => {
+        console.log('Subscribe 1 =>', message)
+    })
+
+
+setTimeout(() => {
+    subscription.unsubscribe();
+    source$
+        .pipe(pluck('data'))
+        .subscribe((message) => {
+            console.log('Subscribe 2 =>', message)
+        })
+}, 7000)
