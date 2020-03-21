d155f18a9a01cfd4209b3307161b4615cb59290f
Ihor <igornepipenko@gmail.com>
2020-03-21 11:36:48 +0200

    Feature: error retry [ref:no-ref]

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { interval, of, zip } from "rxjs";
-import { map } from "rxjs/operators";
+import { catchError, delay, map, retry, retryWhen, tap } from "rxjs/operators";
 
 const sequence1$ = interval(1000);
 const sequence2$ = of('1', '2', '3', 4, '5', '6', '7');
@@ -15,12 +15,21 @@ sequence
             //     console.log(err);
             //     return  'N'
             // }
-        })
+        }),
+        // retry(3),
+        retryWhen((obs) => obs.pipe(delay(3000))),
+        // catchError((err, obs) => {
+        //     console.log(err);
+        //     return of('N')
+        // }),
+        // tap((v)=>{
+        //    console.log('IN TAP', v);
+        // })
     )
     .subscribe((value) => {
         console.log(value);
     }, (err) => {
-        console.log(err);
+        console.log('ERROR 1 =>', err);
     }, () => {
         console.log('Completed')
     })
